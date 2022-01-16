74337190695c457ff53352572de6ce76830a442d
Yamil Villegas <YTorres87@gmail.com>
2022-01-16 18:34:18 -0500

    Node Callbacks and Error Handling with Comments

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ function solveRect(l, w) {
 
     // Note that I used the backticks in the console log because we are using template literals
 
-    if (l <= 0 || w <= 0) {
-        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-    } else {
-        console.log(`Area of rectangle: ${rect.area(l, w)}`);
-        console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-    }
-}   // Since we cannot have a Rectangle with the dimensions zero or smaller we wrote an IF STATEMENT to let the user know the dimensions must be greater than zero. In the else block we use the area & perimeter modules that we created in rectangle.js to figure out the solveRect below. We can see the answers using Node App in out Bash terminal.
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message);
+        // First we call the rect function. The function expects 3 arguments. Two positive numbers and a call back function which are the l, w & (err, rectangle). If there is an error it will show ERROR & the error message inside of the IF STATEMENT in rectangle.js
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+        // IF there is NOT an error then it proceeds to the else block. We console log a message with template literals to let the user know the Area of the rectangle. Inside the template literal at the end of the console log we call on rectangle as defined in the parameter list so that it can pull the data from the .area() function which is located in the set time out function which is inside the else block which is inside of rectangle.js
+    });
+    console.log('This statement is logged after the call to rect()');
+}   // This console log helps us see the order in which the code was executed. This will show for two seconds before the else block executes. 
 
 solveRect(2, 4);
 solveRect(3, 5);
 solveRect(0, 5);
 solveRect(5, -3);
+
+// CALLBACKS AND ERROR HANDLING IN NODE APPLICATIONS
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,15 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+module.exports = (x, y, callback) => {
+    // Since we are using a function as the value for the entire export we must use module.exports instead of the shorthand way to export the module. 
+    if (x <= 0 || y <= 0) {
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+    }   // In the IF STATEMENT we are implementing error handling. We are going to check for the error condition. We run a check to see if x is less than or equal to zero and if y is less than or equal to zero. If it is then we run an Error object with the new keyword, we then add in a message. 
+    else {
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            }),
+            2000
+        );
+    }
+}; // IF it DOES NOT have any errors we use setTimeout() and pass it a function that contains the callback with the argument null which means there is no error. The second argument in the callback function is an object that contains perimeter and area. We finish the setTimeout() function by passing in 2000 which is how many milliseconds to wait.
